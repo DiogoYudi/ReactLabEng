b2d0e26c08396ba9e512bd054b73a02cc5923f2a
DiogoYudi <130068159+DiogoYudi@users.noreply.github.com>
2023-10-27 08:54:32 -0300

    Add files via upload

--- src/Contador.js
+++ src/Contador.js
@@ -3,42 +3,72 @@ import './Contador.css';
 function Contador(){
     let [c, setCountH] = useState(0);
     let [c1, setCountM] = useState(0);
-    let [c2, setCountT] = useState(0);
 
     function handleClickM(){
         setCountM(c1+1);
+    }
+
+    function handleClickMM(){
+        setCountM(c1-1);
     }
 
     function handleClickH(){
         setCountH(c+1);
     }
 
-    function handleClickT(){
-        setCountT(c2+1);
+    function handleClickHM(){
+        setCountH(c-1);
     }
+
     return (
         <>
-            <h3> Total </h3>
-            <p>{c1+c}</p>
-            <div class="mybutton">
-                <MyButton count={c} s={"Man"} onClick={handleClickH}/>
-                <MyButton count={c1} s={"Woman"} onClick={handleClickM}/>
+            <div class="total">
+                <h3> Total </h3>
+                <p>{c1+c}</p>
+            </div>
+            <div class="all">
+                <div class="mybutton">
+                    <div class="a">
+                        <MyButton count={c} s={"+"} onClick={handleClickH}/>
+                        <MyButton count={c} s={"-"} onClick={handleClickHM}/>
+                    </div>
+                    <p>{c}</p>
+                </div>
+                <div class="mybutton">
+                    <div class="a">
+                        <MyButton count={c1} s={"+"} onClick={handleClickM}/>
+                        <MyButton count={c1} s={"-"} onClick={handleClickMM}/>
+                    </div>
+                    <p>{c1}</p>
+                </div>
             </div>
         </>
     );
 }
 
 function MyButton({count, s, onClick}){
-    return (
-        <>
-            {s}
-            <div class="button">
-                <button onClick={onClick}>
-                    Clicked {count} times
-                </button>
-            </div>
-        </>
-    );
+    if(s == "+"){
+        return (
+            <>
+                <div class="button">
+                    <button onClick={onClick}>
+                        +
+                    </button>
+                </div>
+            </>
+        );
+    }
+    if(s == "-"){
+        return (
+            <>
+                <div class="button">
+                    <button onClick={onClick}>
+                        -
+                    </button>
+                </div>
+            </>
+        );
+    }
 }
 
 export default Contador;
