95450146815e1c46851f6b754371288c5ebf59e8
DiogoYudi <130068159+DiogoYudi@users.noreply.github.com>
2023-11-11 10:59:34 -0300

    Delete src/MemoryGame2 directory

--- src/MemoryGame2/App.js
+++
@@ -1,9 +0,0 @@
-import JogoDaMemoria from "./components/JogoDaMemoria";
-
-export default function App(){
-    return(
-        <div class="App">
-            <JogoDaMemoria />
-        </div>
-    )
-}
